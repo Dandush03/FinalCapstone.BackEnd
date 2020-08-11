d8397f23a7d74dc24760779f21cd5f6c3da8691d
Daniel Laloush <d.laloush@outlook.com>
2020-08-11 12:10:03 -0400

    Fixing Schema for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'http://localhost:3000/'
+     origins '*'
 
      resource '/api/*',
        headers: :any,
