4e42ec7638157d2479356550f99a66025a2794f6
Daniel Laloush <d.laloush@outlook.com>
2020-08-11 16:24:20 -0400

    Adding cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,10 +8,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-    origins '*'
+     origins '*'
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch]
+     resource '*',
+       headers: :any,
+       methods: [:get, :post, :put, :patch, :delete, :options, :head]
    end
 end
